d6ed05c7be6c0dd61d3f6fb1c36b7e6b9f6ceb8c
Helena Dalmazo <helenadalmazo@outlook.com>
2021-01-01 22:24:14 -0300

    Adding bottom navigator into group screen

--- lib/screen/group_screen.dart
+++ lib/screen/group_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:despesa_app/model/group_model.dart';
 import 'package:despesa_app/repository/group_repository.dart';
 import 'package:flutter/material.dart';
@@ -15,7 +14,14 @@ class GroupScreen extends StatefulWidget {
 class _GroupScreenState extends State<GroupScreen> {
 
   Group group;
+
   bool loading = true;
+
+  int _bottomNavigationCurrentIndex = 0;
+
+  PageController _pageController = PageController(
+    initialPage: 0
+  );
 
   @override
   void initState() {
@@ -35,14 +41,83 @@ class _GroupScreenState extends State<GroupScreen> {
     });
   }
 
+  void _onPageChanged(int index) {
+    setState(() {
+      _bottomNavigationCurrentIndex = index;
+    });
+  }
+
+  void _onTapButtonNavigation(int index) {
+    setState(() {
+      _bottomNavigationCurrentIndex = index;
+      _pageController.animateToPage(
+        index,
+        duration: Duration(milliseconds: 500),
+        curve: Curves.ease
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          group != null ? group.name : ''
-        )
-      )
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                top: 32,
+                right: 32,
+                bottom: 16,
+                left: 32
+              ),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColorLight,
+              ),
+              child: Text(
+                loading ? '' : group.name,
+                style: Theme.of(context).textTheme.headline5
+              ),
+            ),
+            Expanded(
+              child: PageView(
+                controller: _pageController,
+                onPageChanged: _onPageChanged,
+                children: <Widget>[
+                  Center(
+                    child: Text('Inicial')
+                  ),
+                  Center(
+                    child: Text('Despesas')
+                  ),
+                  Center(
+                    child: Text('Usuários')
+                  ),
+                ],
+              )
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTapButtonNavigation,
+        currentIndex: _bottomNavigationCurrentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicial',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_money),
+            label: 'Despesas',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Usuários'
+          )
+        ],
+      ),
     );
   }
 }
