ca7de1663b258c3b3c29abc3dde3c717ed6adbc6
Helena Dalmazo <helenadalmazo@outlook.com>
2021-02-20 11:17:14 -0300

    Removing unused import

--- lib/widget/list_header.dart
+++ lib/widget/list_header.dart
@@ -1,5 +1,4 @@
 import 'package:despesa_app/clipper/isosceles_trapezoid_clipper.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ListHeader extends StatelessWidget {
