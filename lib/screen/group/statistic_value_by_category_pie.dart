12289dad40404714ea06e2a3f3009801d05eda78
Helena Dalmazo <helenadalmazo@outlook.com>
2021-04-16 22:26:47 -0300

    Adding empty state to statistics pie charts

--- lib/screen/group/statistic_value_by_category_pie.dart
+++ lib/screen/group/statistic_value_by_category_pie.dart
@@ -15,51 +15,68 @@ class StatisticValueByCategoryPieChart extends StatelessWidget {
     @required this.animate,
     @required this.statisticValueByCategory
   }) : super(key: key) {
-    _totalValue = statisticValueByCategory.map((statistic) => statistic.value).reduce((value, element) => value + element);
+    if (statisticValueByCategory.isEmpty) _totalValue = 0;
+    else _totalValue = statisticValueByCategory.map((statistic) => statistic.value).reduce((value, element) => value + element);
+  }
+
+  Widget _getBody(BuildContext context) {
+    if (statisticValueByCategory == null) {
+      return Center(
+        child: CircularProgressIndicator()
+      );
+    }
+
+    if (statisticValueByCategory.isEmpty) {
+      return Center(
+        child: Text(
+          "Nenhum resultado encontrado"
+        )
+      );
+    }
+
+    return charts.PieChart(
+      [
+        charts.Series<StatisticValueGroupedByCategory, String>(
+          id: "statisticValueByCategory",
+          domainFn: (StatisticValueGroupedByCategory statistic, _) => statistic.group,
+          measureFn: (StatisticValueGroupedByCategory statistic, _) => statistic.value,
+          colorFn: (StatisticValueGroupedByCategory statistic, _) {
+            Color color = ExpenseCategory.getColor(statistic.group);
+            return charts.Color(r: color.red, g: color.green, b: color.blue, a: color.alpha);
+          },
+          data: statisticValueByCategory,
+          labelAccessorFn: (StatisticValueGroupedByCategory statistic, _) => PercentageFormat.format(statistic.value/_totalValue * 100),
+        )
+      ],
+      animate: animate,
+      layoutConfig: charts.LayoutConfig(
+        leftMarginSpec: charts.MarginSpec.fixedPixel(0),
+        topMarginSpec: charts.MarginSpec.fixedPixel(0),
+        rightMarginSpec: charts.MarginSpec.fixedPixel(8),
+        bottomMarginSpec:charts.MarginSpec.fixedPixel(0),
+      ),
+      behaviors: [
+        charts.DatumLegend(
+          position: charts.BehaviorPosition.top,
+          outsideJustification: charts.OutsideJustification.startDrawArea,
+          desiredMaxRows: 2,
+        )
+      ],
+      defaultRenderer: charts.ArcRendererConfig(
+        arcWidth: 50,
+        arcRendererDecorators: [
+          charts.ArcLabelDecorator(
+              labelPosition: charts.ArcLabelPosition.inside
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: statisticValueByCategory == null
-            ? Center(child: CircularProgressIndicator())
-            : charts.PieChart(
-          [
-            charts.Series<StatisticValueGroupedByCategory, String>(
-              id: "statisticValueByCategory",
-              domainFn: (StatisticValueGroupedByCategory statistic, _) => statistic.group,
-              measureFn: (StatisticValueGroupedByCategory statistic, _) => statistic.value,
-              colorFn: (StatisticValueGroupedByCategory statistic, _) {
-                Color color = ExpenseCategory.getColor(statistic.group);
-                return charts.Color(r: color.red, g: color.green, b: color.blue, a: color.alpha);
-              },
-              data: statisticValueByCategory,
-              labelAccessorFn: (StatisticValueGroupedByCategory statistic, _) => PercentageFormat.format(statistic.value/_totalValue * 100),
-            )
-          ],
-          animate: animate,
-          layoutConfig: charts.LayoutConfig(
-            leftMarginSpec: charts.MarginSpec.fixedPixel(0),
-            topMarginSpec: charts.MarginSpec.fixedPixel(0),
-            rightMarginSpec: charts.MarginSpec.fixedPixel(8),
-            bottomMarginSpec:charts.MarginSpec.fixedPixel(0),
-          ),
-          behaviors: [
-            charts.DatumLegend(
-              position: charts.BehaviorPosition.top,
-              outsideJustification: charts.OutsideJustification.startDrawArea,
-              desiredMaxRows: 2,
-            )
-          ],
-          defaultRenderer: charts.ArcRendererConfig(
-            arcWidth: 50,
-            arcRendererDecorators: [
-              charts.ArcLabelDecorator(
-                  labelPosition: charts.ArcLabelPosition.inside
-              ),
-            ],
-          ),
-        )
+      child: _getBody(context)
     );
   }
 }
--- lib/screen/group/statistic_value_by_user_pie_chart.dart
+++ lib/screen/group/statistic_value_by_user_pie_chart.dart
@@ -14,51 +14,68 @@ class StatisticValueByUserPieChart extends StatelessWidget {
     @required this.animate,
     @required this.statisticValueByUser
   }) : super(key: key) {
-    _totalValue = statisticValueByUser.map((statistic) => statistic.value).reduce((value, element) => value + element);
+    if (statisticValueByUser.isEmpty) _totalValue = 0;
+    else _totalValue = statisticValueByUser.map((statistic) => statistic.value).reduce((value, element) => value + element);
+  }
+
+  Widget _getBody(BuildContext context) {
+    if (statisticValueByUser == null) {
+      return Center(
+        child: CircularProgressIndicator()
+      );
+    }
+
+    if (statisticValueByUser.isEmpty) {
+      return Center(
+        child: Text(
+          "Nenhum resultado encontrado"
+        )
+      );
+    }
+
+    return charts.PieChart(
+      [
+        charts.Series<StatisticValueGroupedByUser, String>(
+          id: "statisticValueByUser",
+          domainFn: (StatisticValueGroupedByUser statistic, _) => statistic.user.getFirstName(),
+          measureFn: (StatisticValueGroupedByUser statistic, _) => statistic.value,
+          colorFn: (StatisticValueGroupedByUser statistic, _) {
+            Color color = statistic.user.getColor();
+            return charts.Color(r: color.red, g: color.green, b: color.blue, a: color.alpha);
+          },
+          data: statisticValueByUser,
+          labelAccessorFn: (StatisticValueGroupedByUser statistic, _) => PercentageFormat.format(statistic.value/_totalValue * 100),
+        )
+      ],
+      animate: animate,
+      layoutConfig: charts.LayoutConfig(
+        leftMarginSpec: charts.MarginSpec.fixedPixel(0),
+        topMarginSpec: charts.MarginSpec.fixedPixel(0),
+        rightMarginSpec: charts.MarginSpec.fixedPixel(8),
+        bottomMarginSpec:charts.MarginSpec.fixedPixel(0),
+      ),
+      behaviors: [
+        charts.DatumLegend(
+          position: charts.BehaviorPosition.top,
+          outsideJustification: charts.OutsideJustification.startDrawArea,
+          desiredMaxRows: 2,
+        )
+      ],
+      defaultRenderer: charts.ArcRendererConfig(
+        arcWidth: 50,
+        arcRendererDecorators: [
+          charts.ArcLabelDecorator(
+              labelPosition: charts.ArcLabelPosition.inside
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: statisticValueByUser == null
-            ? Center(child: CircularProgressIndicator())
-            : charts.PieChart(
-          [
-            charts.Series<StatisticValueGroupedByUser, String>(
-              id: "statisticValueByUser",
-              domainFn: (StatisticValueGroupedByUser statistic, _) => statistic.user.getFirstName(),
-              measureFn: (StatisticValueGroupedByUser statistic, _) => statistic.value,
-              colorFn: (StatisticValueGroupedByUser statistic, _) {
-                Color color = statistic.user.getColor();
-                return charts.Color(r: color.red, g: color.green, b: color.blue, a: color.alpha);
-              },
-              data: statisticValueByUser,
-              labelAccessorFn: (StatisticValueGroupedByUser statistic, _) => PercentageFormat.format(statistic.value/_totalValue * 100),
-            )
-          ],
-          animate: animate,
-          layoutConfig: charts.LayoutConfig(
-            leftMarginSpec: charts.MarginSpec.fixedPixel(0),
-            topMarginSpec: charts.MarginSpec.fixedPixel(0),
-            rightMarginSpec: charts.MarginSpec.fixedPixel(8),
-            bottomMarginSpec:charts.MarginSpec.fixedPixel(0),
-          ),
-          behaviors: [
-            charts.DatumLegend(
-              position: charts.BehaviorPosition.top,
-              outsideJustification: charts.OutsideJustification.startDrawArea,
-              desiredMaxRows: 2,
-            )
-          ],
-          defaultRenderer: charts.ArcRendererConfig(
-            arcWidth: 50,
-            arcRendererDecorators: [
-              charts.ArcLabelDecorator(
-                labelPosition: charts.ArcLabelPosition.inside
-              ),
-            ],
-          ),
-        )
+      child: _getBody(context)
     );
   }
 }
